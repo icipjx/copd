4fbd5cb69219938d791d2737b29a2f7c09e21a7e
icipjx <icipjx@gmail.com>
2018-06-25 08:17:50 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+#encoding:utf-8
+import time
+import csv
+import pandas as pd
+
+# a = "2015-03-18 00:00:00"
+# timeArray = time.strptime(a, "%Y-%m-%d %H:%M:%S")
+# otherStyleTime = time.strftime("%Y%m%d", timeArray)
+# print(otherStyleTime)
+#
+# d = pd.read_csv('G:\copd\data\copd\chengdu\cd_510104_jinjiang.csv', usecols=['case', 'roi', 'eq. diam.','x loc.','y loc.','slice no.'])
+# print(d)
+
+def merge_pol()
